d169a260a1a9cb684a078c3584c9b32abc974fed
sakibsarker <sakib15-3824@diu.edu.bd>
2024-11-30 22:54:13 +0600

    game card 2 image for mobile

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -50,7 +50,7 @@ export default function GameCard() {
               All Games
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-4">
             {liveGamingProviders.map((provider) => (
               <div
                 key={provider.name}
@@ -96,7 +96,7 @@ export default function GameCard() {
               All Games
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-4">
             {digitalCasinoProviders.map((provider) => (
               <div
                 key={provider.name}
